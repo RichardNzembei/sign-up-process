71c30c9b975d4080c3be156247a4dd202ad4e886
RichardNzembei <richardsonreuben78@gmail.com>
2024-04-11 16:31:55 +0300

    update

--- public/app.js
+++ public/app.js
@@ -24,7 +24,7 @@ const connection = mysql.createConnection({
 
 connection.connect((error) => {
     if (error) {
-        throw error;
+        console.error("Error connecting:", error);
     } else {
         console.log("Connection successful");
     }
